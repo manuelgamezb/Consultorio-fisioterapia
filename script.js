1c50e63d4fc58a13eda163fba54c4c78644097f6
Estivaliz gamez <manuelgamezb@gmail.com>
2025-12-28 13:46:51 -0700

    dia 3 completo

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
-function irAPacientes(){
-    //mostrar la seccion de pacientes//
-    document.getElementById("seccionPacientes").style.display = "block";
-    }
-function cerrarFormulario() {
-    //ocultar seccion pacientes//
-    document.getElementById("seccionPacientes").style.display = "none";
+//array para guardar todos los pacientes//
+let pacientes =[];
+function irAPacientes() {
+document.getElementById("seccionPacientes").style.display = "block";
+}
+
+function cerrarFormulario () {
+    alert("la funcion se ejecuto");
+    document.getElementById("seccionPacientes").style.display ="none";
     
 }
 function guardarPaciente(){
     //obtener los valores//
-    let nombre= document.getElementById("nombrePaciente").value;
+    let nombre = document.getElementById("nombrePaciente").value;
     let telefono = document.getElementById("telefonoPaciente").value;
+    //validar que no esten vacios//
+    if (nombre ==="" || telefono ===""){
+        alert("Por favor llena todos los campos");
+        return;
+    }
+    //crear objeto paciente//
+    let paciente = {
+        nombre: nombre,
+        telefono: telefono
+    };
 
-    //mostrar datos//
-    alert("Paciente guardado:\nNombre: "  + nombre + "\nTelefono: "  + telefono);
-    //limpiar el formulario//
-    document.getElementById("nombrePaciente").value= "";
-    document.getElementById("telefonoPaciente").value = "";
-    
+    //agregar array//
+    pacientes.push(paciente);
+
+    //mostrar en la tabla//
+    mostrarPacientes();
+//limpiar el formulario//
+document.getElementById("nombrePaciente").value = "";
+document.getElementById("telefonoPaciente").value = "";
+
+alert("Paciente Guardado exitosamente");
+
+}
+function mostrarPacientes() {
+    let tbody = document.getElementById("listaPacientes");
+    tbody.innerHTML = ""; 
+
+    //recorrer todos  los pacientes//
+
+    for(let i = 0; i<pacientes.length; i++) {
+        let fila = "<tr><td>" + pacientes[i].nombre + "</td><td>" + pacientes[i].telefono + "</td></tr>";
+        tbody.innerHTML += fila;
+    }
+
+
 }
